4e3098084c0cddcf8cc1f579d846e90b549bdced
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-22 21:21:02 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") we are using nodejs not browser
+
+console.log(3+3)
+
+
+//primitive data types
+let name="abc" //string ("")
+let age=18 //num (2 to power 53) bigint
+let ispresent=false //bool (true, false)
+//null =>standalone value (represntation of null value)
+//undefined ->value is not defined
+//symbol ->uniqueness
+
+//object
+console.log(typeof "abc")
+
