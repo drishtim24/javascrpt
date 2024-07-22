e830609841eaeb46d9fb69fc4b0eb7e80048b283
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-22 21:56:58 +0000

    conversions

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+let score="33"
+
+console.log(typeof score)
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber)
+let isloggedin = 1
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+//1->true
+//0->false
+//""->false
+//"abc"->true
+
+let somenumber=33
+let stringnum=String(somenumber)
+console.log(stringnum)
+console.log(typeof stringnum)
