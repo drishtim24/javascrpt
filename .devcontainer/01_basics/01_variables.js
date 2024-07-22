ea5b0030b7ba107e65ba8068f26b3cae5ac727ba
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-22 21:07:25 +0000

    config data done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553 //cannot be changed
+let accountEmail="drishti@google.com" //can be changed
+var accountPassword="12345" //can be changed
+accountCity="jalandhar" //can be changed
+let accountState
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
+//dont use var due to block scope and functional scope problem
