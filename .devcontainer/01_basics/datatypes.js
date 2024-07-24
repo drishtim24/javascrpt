a118582ec242dd946932a69b6bb523e9ba5f333f
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-24 08:59:51 +0000

    stack and heap

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -6,3 +6,21 @@ const score=100
 //Refernce (non primitive)
 
 //Array, objects, functions
+
+
+//stack(primitve) ->makes a copy, heap(non primitive)->makes a refernce
+let myname="drishti"
+let anothername=myname
+anothername="chaiaurcode"
+console.log(myname)
+console.log(anothername)
+
+let user={
+    email:"user@google.com",
+    upi: "user@byl"
+}
+
+let usertwo=user
+usertwo.email="drishti@google.com"
+console.log(user.email); // it will be changed
+console.log(usertwo.email) //it will be changed
