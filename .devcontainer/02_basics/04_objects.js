1153a8182d6f054f746c8c63154160e0efb11f68
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-25 20:56:34 +0000

    objects

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,13 @@
+//const tinderuser=new Object() //singleton object
+const tinderuser={} //non singleton object
+
+tinderuser.id="123abc"
+tinderuser.name="sammy"
+tinderuser.isloggedin=false
+
+//console.log(tinderuser)
+
+const regularuser={
+    
+}
+
