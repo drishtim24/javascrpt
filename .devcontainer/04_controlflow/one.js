97ab45150a708c94a46e7fdcd4604ca78df1e883
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-29 14:38:56 +0000

    if else

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,47 @@
+const isuserloggedin=true
+
+const temp=41
+
+// if(temp<50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temp is greater than 50");
+// }
+
+if(2==="2"){
+    console.log("executed");
+} //=== checks the type also
+
+const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power}`);
+// }
+
+const balance=1000
+//if(balance>500) console.log("test"), console.log("test2");;
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("more than 750");
+// }
+
+const userloggedin=true
+const debitcard=true
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userloggedin && debitcard){
+    console.log("allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
