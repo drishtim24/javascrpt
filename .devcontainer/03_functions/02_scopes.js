aff0dd977b5e52b05c821b05908f056e0a53909c
drishtim24 <160578170+drishtim24@users.noreply.github.com>
2024-07-29 12:14:02 +0000

    scopes

--- .devcontainer/03_functions/02_scopes.js
+++ .devcontainer/03_functions/02_scopes.js
@@ -5,4 +5,39 @@ if(true){
 }
 //console.log(a);
 //console.log(b);
-console.log(c); //c will be printed
+//console.log(c); //c will be printed
+
+function one(){ //parent functions
+    const username="hitesh"
+    function two(){ //child function
+        const website="youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website=" youtube"
+        //console.log(username+website)
+    }
+    //console.log(website);
+}
+//console.log(uername);
+
+
+
+
+//+++++++++++intersting++++++++++
+function addone(num){
+    return num+1
+}
+console.log(addone(5)) //it can run without and error if we move it upwards
+
+const addtwo =function(num){
+    return num+2;
+}
+addtwo(5) //it will throw an error if we move it upward
